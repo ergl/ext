162fc995a45df25d2ce532a0bb02c7dd9cbf5375
ergl <ergl@users.noreply.github.com>
2021-10-29 10:04:11 +0200

    Delay setting init_node until first request

--- src/ext.erl
+++ src/ext.erl
@@ -45,7 +45,7 @@
     id :: binary(),
     timestamp :: timestamp(),
     threshold_timestamp :: timestamp(),
-    init_node :: {node_ip(), inet:port_number()},
+    init_node = undefined :: undefined | {node_ip(), inet:port_number()},
     leaders = #{} :: #{partition_id() => replica_id()},
     ballots = #{} :: #{partition_id() => ballot()}
 }).
@@ -53,8 +53,8 @@
 -opaque t() :: #coordinator{}.
 -opaque tx() :: #transaction{}.
 
--opaque read_req_id() :: {read, shackle:external_request_id(), partition_id()}.
--opaque update_req_id() :: {update, shackle:external_request_id(), partition_id()}.
+-opaque read_req_id() :: {read, shackle:external_request_id(), partition_id(), {node_ip(), inet:port_number()}}.
+-opaque update_req_id() :: {update, shackle:external_request_id(), partition_id(), {node_ip(), inet:port_number()}}.
 
 -export_type([t/0, tx/0, read_req_id/0, update_req_id/0]).
 
@@ -93,15 +93,16 @@ new(ReplicaId, LocalIP, WorkerId, RingInfo, NodePool) ->
 %%====================================================================
 
 -spec start_transaction(t(), non_neg_integer()) -> {ok, tx()}.
-start_transaction(#coordinator{self_ip=Ip, coordinator_id=LocalId, replica_id=ReplicaId, ring=Ring}, Id) ->
+start_transaction(#coordinator{self_ip=Ip, coordinator_id=LocalId, replica_id=ReplicaId}, Id) ->
     TxId = make_id(ReplicaId, Ip, LocalId, Id),
     Ts0 = erlang:system_time(nanosecond),
     %% FIXME(borja, time): Clock skew?
-    Ts1 = Ts0,
-    {_, Idx} = ext_ring:random_partition(Ring),
-    {ok, #transaction{id=TxId, init_node=Idx, timestamp=Ts0, threshold_timestamp=Ts1}}.
+    {ok, #transaction{id=TxId, timestamp=Ts0, threshold_timestamp=Ts0}}.
 
 -spec commit(t(), tx()) -> ok | error.
+commit(_, #transaction{init_node=undefined}) ->
+    %% If the transaction never read anything, we bypass 2PC
+    ok;
 commit(#coordinator{conn_pool=Pools}, #transaction{id=TxId, init_node=Idx, ballots=Ballots}) ->
     Pool = maps:get(Idx, Pools),
     ext_shackle_transport:commit(Pool, TxId, Ballots).
@@ -113,15 +114,19 @@ async_read(#coordinator{ring=Ring, conn_pool=Pools},
     {P, Idx} = ext_ring:get_key_location(Ring, Key),
     Pool = maps:get(Idx, Pools),
     {ok, ReqId} = ext_shackle_transport:read_request(Pool, maps:get(P, Leaders, empty), TxId, Ts, Key),
-    {ok, {read, ReqId, P}}.
+    {ok, {read, ReqId, P, Idx}}.
 
 -spec await_read(t(), tx(), read_req_id()) -> {ok, binary(), tx()} | error.
-await_read(_, Tx=#transaction{ballots=Ballots}, {read, ReqId, P}) ->
+await_read(_, Tx=#transaction{ballots=Ballots}, {read, ReqId, P, Idx}) ->
     case shackle:receive_response(ReqId) of
         error ->
             error;
         {ok, Ballot, ShardLeader, Value} ->
-            {ok, Value, Tx#transaction{ballots=Ballots#{P => Ballot}, leaders=#{P => ShardLeader}}}
+            {
+                ok,
+                Value,
+                set_tx_init_node(Tx#transaction{ballots=Ballots#{P => Ballot}, leaders=#{P => ShardLeader}}, Idx)
+            }
     end.
 
 -spec async_update(t(), tx(), binary(), binary()) -> {ok, update_req_id()}.
@@ -132,15 +137,18 @@ async_update(#coordinator{ring=Ring, conn_pool=Pools},
     {P, Idx} = ext_ring:get_key_location(Ring, Key),
     Pool = maps:get(Idx, Pools),
     {ok, ReqId} = ext_shackle_transport:update_request(Pool, maps:get(P, Leaders, empty), TxId, Ts, Key, Value),
-    {ok, {update, ReqId, P}}.
+    {ok, {update, ReqId, P, Idx}}.
 
 -spec await_update(t(), tx(), update_req_id()) -> {ok, tx()} | error.
-await_update(_, Tx=#transaction{ballots=Ballots}, {update, ReqId, P}) ->
+await_update(_, Tx=#transaction{ballots=Ballots}, {update, ReqId, P, Idx}) ->
     case shackle:receive_response(ReqId) of
         error ->
             error;
         {ok, Ballot, ShardLeader} ->
-            {ok, Tx#transaction{ballots=Ballots#{P => Ballot}, leaders=#{P => ShardLeader}}}
+            {
+                ok,
+                set_tx_init_node(Tx#transaction{ballots=Ballots#{P => Ballot}, leaders=#{P => ShardLeader}}, Idx)
+            }
     end.
 
 -spec sync_read(t(), tx(), binary()) -> {ok, binary(), tx()} | error.
@@ -157,11 +165,14 @@ sync_update(Coord, Tx, Key, Value) ->
 %% Internal functions
 %%====================================================================
 
+-spec set_tx_init_node(tx(), {node_ip(), inet:port_number()}) -> tx().
+set_tx_init_node(Tx=#transaction{init_node=undefined}, Idx) -> Tx#transaction{init_node=Idx};
+set_tx_init_node(Tx, _) -> Tx.
+
 %%====================================================================
 %% Util functions
 %%====================================================================
 
-%% TODO(borja): Rethink
 -spec make_id(binary(), binary(), non_neg_integer(), non_neg_integer()) -> binary().
 make_id(ReplicaId, Ip, LocalId, Id) ->
     <<
